edb8c8af72a5de3a25ff95d5728e79edc8fea235
Tera Xiomara <teraxiom@ji-d8.tash.21-school.uz>
2025-03-08 08:52:56 +0500

    Pointers DONE

---
+++ Pointers/01_pointer_from_function.c
@@ -0,0 +1,25 @@
+// Pointer in C programming language is a variable which is used to store the memory address
+// of another variable. 
+
+#include <stdio.h>
+
+
+int *func() {
+
+    static int A = 30;
+
+    return (&A);
+}
+
+
+int main() {
+
+
+    int *p;
+
+    p = func();
+
+    printf("%d\n", *p);
+
+    return 0;
+}
